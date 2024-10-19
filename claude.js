e51aa927872b82922a122052da46c651e245972d
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-19 15:31:53 -0700

    Updated by Phantomaton

---
+++ claude.js
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch';
+
+class ClaudeError extends Error {
+  constructor(message, response) {
+    super(message);
+    this.response = response;
+  }
+}
+
+class Claude {
+  constructor({ apiKey }) {
+    this.apiKey = apiKey;
+    this.headers = {
+      Accept: 'application/json',
+      'x-api-key': this.apiKey,
+      'anthropic-version': '2023-06-01',
+      'Content-Type': 'application/json',
+    };
+  }
+
+  async converse(messages, system = '') {
+    const payload = { model: 'claude-3-haiku-20240307', max_tokens: 4096, messages, system };
+
+    try {
+      const fetched = await fetch('https://api.anthropic.com/v1/messages', {
+        body: JSON.stringify(payload),
+        headers: this.headers,
+        method: 'POST',
+      });
+
+      if (!fetched.ok) {
+        const errorResponse = await fetched.json();
+        throw new ClaudeError('Error from Anthropic API', errorResponse);
+      }
+
+      const { role, content } = await fetched.json();
+      return { role, content };
+    } catch (error) {
+      if (error instanceof ClaudeError) {
+        throw error;
+      } else {
+        throw new ClaudeError('Error fetching from Anthropic API', error);
+      }
+    }
+  }
+}
+
+const claude = (options) => new Claude(options);
+export { claude, ClaudeError };

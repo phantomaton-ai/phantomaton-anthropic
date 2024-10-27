52e86989983af63fc0b010be53e60c01edb49a98
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-26 18:40:44 -0700

    phantomaton-anthropic

---
+++ phantomaton-anthropic.js
@@ -0,0 +1,31 @@
+import conversations from 'phantomaton-conversations';
+import system from 'phantomaton-system';
+import claude from './claude.js';
+
+class Assistant {
+  constructor(instance, system) {
+    this.instance = instance;
+    this.system = system;
+  }
+
+  async converse(turns) {
+    const messages = turns.map(({ message, reply }) => [
+      { role: 'user', content: message },
+      { role: 'assistant', content: reply }
+    ]);
+    const system = this.system();
+    return this.instance.converse(messages, system);
+  }
+}
+
+const anthropic = (options) => {
+  const instance = claude(options);
+  instance.install = [
+    conversations.assistant.provider(
+      [system.system.resolve]
+      (system) => new Assistant(instance, system)
+    )
+  ]
+];
+
+export default anthropic;
